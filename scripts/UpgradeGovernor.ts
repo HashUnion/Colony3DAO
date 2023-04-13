7ea81aed2c0438a66e17ad18243a715b42533e6a
DwGoing <jianghy1209@163.com>
2023-04-13 18:36:37 +0800

    🛠 miscellaneous: Change names of somethings

--- scripts/UpgradeGovernor.ts
+++ scripts/UpgradeGovernor.ts
@@ -5,18 +5,18 @@ async function main() {
   const proxyAddress = process.env.PROXY_ADDRESS as string;
   if (proxyAddress == undefined || proxyAddress.length <= 0) throw Error("INVAILD PROXY_ADDRESS")
 
-  const GovernorBase = await ethers.getContractAt("GovernorBase", proxyAddress);
-  const version = +(await GovernorBase.version()) + 1;
+  const Upgradeable = await ethers.getContractAt("Upgradeable", proxyAddress);
+  const version = +(await Upgradeable.version()) + 1;
   const Governor = await ethers.getContractFactory(`GovernorV${version}`);
   const implementationAddress = await upgrades.prepareUpgrade(proxyAddress, Governor, { kind: 'uups' });
-  console.info(`GovernorV${version} Deployed ===> ${implementationAddress.valueOf()}`);
+  console.info(`Governor v${version} Deployed ===> ${implementationAddress.valueOf()}`);
 
   const verifyGovernor = new Promise(f => setTimeout(f, 5000)).then(async () => {
     await hardhat.run("verify:verify", {
       address: implementationAddress,
       constructorArguments: [],
     });
-    console.info(`GovernorV${version} Verfied`)
+    console.info(`Governor v${version} Verfied`)
   }).catch((error) => console.error(error));
 
   await verifyGovernor;
